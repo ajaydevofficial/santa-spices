c085dc6e56fe14eb8d25528332483353894f34a1
ajaydevofficial <ajaydevofficial@gmail.com>
2020-08-09 22:05:34 +0530

    firebase function added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.addProductCategory = functions.https.onRequest((request, response) => {
+  if(request.method !== "POST"){
+    response.send(405, 'HTTP Method ' +request.method+' not allowed');
+  }
+  else{
+    console.log(`Request: ${request.body}`);
+    productName = request.body.product;
+    console.log(`Product Name: ${productName}`);
+    admin.database().ref('products/').push({
+      product:productName
+    }).then(()=>{
+      response.send({
+          status:200,
+          message:"Successfully added new product"
+      })
+    }).catch((err)=>{
+        console.log(err)
+        response.send({
+            status:403,
+            message: err
+        })
+    })
+  }
+});
+
