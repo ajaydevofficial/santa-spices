97387e2371006cbabd098ab5c72043d76d57c10d
Ajay Dev D <42738646+ajaydevofficial@users.noreply.github.com>
2020-09-19 12:07:27 +0530

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7eZpornIHq37d4nynsIFxSpd3uiXp-p0",
-    authDomain: "santa-spices.firebaseapp.com",
-    databaseURL: "https://santa-spices.firebaseio.com",
-    projectId: "santa-spices",
-    storageBucket: "santa-spices.appspot.com",
-    messagingSenderId: "305714224867",
-    appId: "1:305714224867:web:2e583e6fb31aeb7bc8efd6",
-    measurementId: "G-5B3GJZNQTY"
+    apiKey: "xxxxx",
+    authDomain: "xxxxxx",
+    databaseURL: "xxxxxx",
+    projectId: "xxxxx",
+    storageBucket: "xxxxxx",
+    messagingSenderId: "xxxx",
+    appId: "xxxxxx",
+    measurementId: "xxxxxx"
   }
 
 const app = firebase.initializeApp(firebaseConfig)
